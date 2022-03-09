86fc93e9f22e53817921d7a7609821690daf9412
mshad <nanicku@gmail.com>
2022-03-09 15:20:45 +0300

    start2

---
+++ cub3d.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshad <mshad@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 13:57:01 by mshad             #+#    #+#             */
+/*   Updated: 2022/03/09 14:28:10 by mshad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <string.h>
+# include <mlx.h>
+# include "gnl/get_next_line.h"
+# include "libft/libft.h"
+
+#endif
